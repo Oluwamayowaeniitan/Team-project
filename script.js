d23a8c6149ba0b52453d3e5d0425bee7f8e0c8cd
Oluwamayowaeniitan <oluwamayowaeniitan@gmail.com>
2023-01-01 18:42:36 +0100

    new commit

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 // The website  should be responsive at 965px
 var dropdown = document.getElementById('drop-down');
+var dropdownclass = document.querySelector('.drop-down');
 
 function sidebar(){
-    
+    dropdown.classList.toggle('show');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function showmenu(){
+//     if(sidebar()){
+//         dropdown.style.display = "none";
+//     }
+// }
+
+
+// dropdownclass.addEventListener('click', showmenu);
